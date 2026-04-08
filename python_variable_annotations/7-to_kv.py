82d53cfbaa8181fc8a4768745261449ecd029624
Bengin05 <uzunbengin9@gmail.com>
2026-04-08 08:29:00 +0000

    feat: add the style pycodestyle and edit the def

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""Type-annotated function to_kv"""
+"""Type-annotated function to_kv."""
 
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> float:
-    """Return a tuple with string and square of int/float"""
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Return a tuple with string and square of int/float."""
     return (k, v ** 2)

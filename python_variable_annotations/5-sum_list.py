234010a488313582b7e9fa164edf5345bbdcc74f
Bengin05 <uzunbengin9@gmail.com>
2026-04-08 08:22:21 +0000

    feat: add the style pycodestyle

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""Type-annotated function sum_list"""
+"""Type-annotated function sum_list."""
 
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
-    """Sum a list of floats"""
+    """Sum a list of floats."""
     return sum(input_list)

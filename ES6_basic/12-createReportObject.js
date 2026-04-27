c904b913cd6a3fb0969da34b53906c3c58d4bbe5
Bengin05 <uzunbengin9@gmail.com>
2026-04-27 13:08:33 +0000

    index: Implement createReportObject function
    - Returns an object with allEmployees key mapping to employeesList
    - Adds getNumberOfDepartments method to count departments
    - Uses ES6 method syntax

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,12 @@
+import createEmployeesObject from './11-createEmployeesObject.js';
+
+export default function createReportObject(employeesList) {
+  // eslint-disable-next-line no-unused-vars
+  const unused = createEmployeesObject;
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employees) {
+      return Object.keys(employees).length;
+    },
+  };
+}

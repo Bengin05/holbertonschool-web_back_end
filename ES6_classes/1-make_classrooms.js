7fc7ef46e6c064a71f4f2cefc29dbdf661a12a59
Bengin05 <uzunbengin9@gmail.com>
2026-04-28 09:28:55 +0000

    feat: initialize rooms with predefined classroom sizes
    * Import `ClassRoom` from `0-classroom.js`
    * Add `initializeRooms` function
    * Return an array of 3 `ClassRoom` instances with sizes:
    
      * 19
      * 20
      * 34

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,13 @@
+export class ClassRoom {
+    constructor(maxStudentsSize) {
+        this._maxStudentsSize = maxStudentsSize;
+    }
+}
+
+function initializeRooms() {
+    const sizes = [19,20,34];
+    const newArr = sizes.map(size => new ClassRoom(size));
+    return newArr
+}
+
+export default initializeRooms

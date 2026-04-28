5740a7c1da17d67a4de91469a45a43d49692eadb
Bengin05 <uzunbengin9@gmail.com>
2026-04-28 10:08:44 +0000

    feat: implement HolbertonCourse class with validation
    * Create `HolbertonCourse` class
    * Add constructor with:
    
      * `name` (String)
      * `length` (Number)
      * `students` (Array of Strings)
    * Validate attribute types during instantiation
    * Store values using underscored properties:
    
      * `_name`
      * `_length`
      * `_students`
    * Implement getters and setters for all attributes
    * Enforce type checking inside setters

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+        this._name = name
+        this._length = length
+        this._students = students
+    }
+    //Getters
+    get name() {
+        return this._name;
+    }
+
+    get length() {
+        return this._length;
+    }
+
+    get students() {
+        return this._students;
+    }
+    //Setters with type verification
+    set name(value) {
+        if (typeof value !== "string") {
+            throw new TypeError("Name must be a string");
+        }
+        this._name = value
+    }
+
+    set length(value) {
+        if (typeof value !== "number" || Number.isNaN(value)) {
+            throw new TypeError("Length must be an integer")
+        }
+        this._length = value
+    }
+
+    set students(value) {
+        if (!Array.isArray(value)) {
+            throw new TypeError("Students need to be an array");
+        }
+        for (const student of value) {
+            if (typeof student !== "string") {
+                throw new TypeError("Student need to be a string");
+            }
+        }
+        this._students = value
+    }
+}

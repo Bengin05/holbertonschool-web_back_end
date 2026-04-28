8b93b58042557da13c2beb987047583c48b19e74
Bengin05 <uzunbengin9@gmail.com>
2026-04-28 13:58:19 +0000

    feat: implement Car class with cloning support
    * Create `Car` class
    * Add constructor with:
    
      * `brand` (String)
      * `motor` (String)
      * `color` (String)
    * Store attributes using underscored properties:
    
      * `_brand`
      * `_motor`
      * `_color`
    * Implement `cloneCar()` method
    * Return a new instance of the class when cloning
    * Use ES6 Symbols for proper class behavior

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,15 @@
+export default class Car {
+    constructor(brand, motor, color) {
+        this._brand = brand
+        this._motor = motor
+        this._color = color
+    }
+
+    toString() {
+        return this._brand;
+    }
+
+    cloneCar() {
+        return new this.constructor();
+    }
+}

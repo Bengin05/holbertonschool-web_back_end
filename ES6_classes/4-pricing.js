4b79b16c267de75b906b53c52dc1fc40e3fcce45
Bengin05 <uzunbengin9@gmail.com>
2026-04-28 12:51:11 +0000

    feat: implement Pricing class with currency support
    * Import `Currency` from `3-currency.js`
    * Create `Pricing` class
    * Add constructor with:
    
      * `amount` (Number)
      * `currency` (Currency)
    * Store attributes using underscored properties:
    
      * `_amount`
      * `_currency`
    * Implement getters and setters for each attribute
    * Add `displayFullPrice()` method
    * Return formatted price as:
    
      * `amount currency_name (currency_code)`
    * Add static `convertPrice(amount, conversionRate)` method
    * Return converted amount using multiplication

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+    constructor(amount, currency) {
+        this._amount = amount
+        this._currency = currency
+    }
+
+    static convertPrice(amount, conversionRate) {
+        return amount * conversionRate;
+    }
+    //Getter
+    get amount() {
+        return this._amount;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+    //Setter with type verification
+    set amount(value) {
+        if (typeof value !== "number" || Number.isNaN(value)) {
+            throw new TypeError("Amount must be an integer")
+        }
+        this._amount = value;
+    }
+
+    set currency(value) {
+        if (typeof value !== "object") {
+            throw new TypeError("Currency must be an object")
+        }
+        this._currency = value;
+    }
+
+    displayFullPrice() {
+        return `${this._amount} ${this._currency.displayFullCurrency()}`
+    } 
+}

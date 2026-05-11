31dc98a4ef680293f483f6be82f57addab0e5336
Bengin05 <uzunbengin9@gmail.com>
2026-05-11 11:05:23 +0000

    Fix http module import for compatibility

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,10 @@
+const { createServer } = require('node:http');
+
+const app = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content_Type', 'text/plain');
+    res.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+module.exports = app;
